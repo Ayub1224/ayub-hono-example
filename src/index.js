34efb37f97e6b33ed84e67d54b79a567c8c794d5
Ayub Khan <khanayubsmart@gmail.com>
2025-06-18 00:09:53 +0530

    Refactor Hono app: update root endpoint response to 'welcome', add POST /params endpoint to handle query parameters and return JSON response.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-import { Hono } from 'hono';
+// import { Hono } from 'hono';
+// const app = new Hono();
+// app.get('/', (resp) => resp.text('hello There !!!'));
+// export const handler = app.fetch;
+
+
+
+import {Hono} from 'hono';
 const app = new Hono();
-app.get('/', (resp) => resp.text('hello There !!!'));
+
+app.get('/', (resp) => resp.text('welcome'));
+
+app.post('/params', async (c) => {
+  const params = c.req.query();
+  return c.json({
+    message: 'Received parameters',
+    params: params
+  });
+});
+
+
 export const handler = app.fetch;
